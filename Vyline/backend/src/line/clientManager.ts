6784a12946acff3b066400b6aff478775fcf552d
nezumi0627 <103794002+nezumi0627@users.noreply.github.com>
2026-09-20 16:20:51 +0900

    fix(account): release message log streams

--- Vyline/backend/src/line/clientManager.ts
+++ Vyline/backend/src/line/clientManager.ts
@@ -35,6 +35,7 @@ import {
   clearAccountRuntimeCaches,
 } from "../service/lineService.js";
 import { releaseAccountChatCache } from "../storage/chatStore.js";
+import { releaseAccountMessageLog } from "../storage/messageLog.js";
 import { loadAccountSettings } from "../service/accountSettingsService.js";
 import { appendDiagnostic } from "../service/diagnosticsService.js";
 import { deactivatePluginsForAccount, restoreEnabledPlugins } from "./pluginManager.js";
@@ -1064,6 +1065,9 @@ export async function removeClient(accountId: string): Promise<void> {
   });
   await releaseAccountChatCache(accountId).catch((err) => {
     log.warn({ accountId, err }, "chat cache release failed after client removal");
+  });
+  await releaseAccountMessageLog(accountId).catch((err) => {
+    log.warn({ accountId, err }, "message log release failed after client removal");
   });
   log.info({ accountId }, "client removed");
 }
